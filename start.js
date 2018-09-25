07198c7bdc4b3ba656e4e7c4617cf8974dcea392
Prabuddha Perera <prabuddha.proart@gmail.com>
2018-09-25 17:47:26 +0530

    heroku updates

--- start.js
+++ start.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-
-
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle an bad connections
-mongoose.connect(process.env.DATABASE);
+var uristring = process.env.DATABASE
+mongoose.connect(uristring);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
